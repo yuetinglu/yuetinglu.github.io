0d536896f80986e9f047eb48d116f76fc50dd7d7
roy3221 <roychu.r@gmail.com>
2019-04-14 18:04:30 -0700

    all together for m09

--- aau/wnm617/m09/javascripts/main.js
+++ aau/wnm617/m09/javascripts/main.js
@@ -1,16 +1,48 @@
-  $(function() {
-    $("#draggable").draggable();
-    $("#droppable").droppable({
+$("#one").ready(function () {
+    $("#accordion").accordion();
+});
+
+$("#two").ready(function () {
+    $("#apple").draggable();
+    $("#banana").draggable();
+    $("#orange").draggable();
+    $("#napkins").draggable();
+    $("#plastic").draggable();
+    $("#wood").draggable();
+
+    $("#composite").droppable({
+   	  accept:("#apple, #orange, #banana, #wood"),
+      tolerance:'touch',
       drop: function(event, ui) {
         $(this)
         .addClass("ui-state-highlight")
         .find("p")
-        .html("Dropped!");
-         console.log("dropped!");
+        .html("You found the right bin!!!");
       }
     });
-  });
 
-    
+    $("#recycle").droppable({
+   	  accept:("#napkins"),
+      tolerance:'touch',
+      drop: function(event, ui) {
+        $(this)
+        .addClass("ui-state-highlight")
+        .find("p")
+        .html("You found the right bin!!!");
+      }
+    });
+
+    $("#recycle").droppable({
+   	  accept:("#plastic"),
+      tolerance:'touch',
+      drop: function(event, ui) {
+        $(this)
+        .addClass("ui-state-highlight")
+        .find("p")
+        .html("You found the right bin!!!");
+              }
+    });
+
+  });
     
     

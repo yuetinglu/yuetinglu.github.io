2cde743183a53582b09d79115a0a970670488f34
yuetinglu <44427526+yuetinglu@users.noreply.github.com>
2019-11-30 01:32:30 -0800

    change user name

--- ArtTour/db_connect.php
+++ ArtTour/db_connect.php
@@ -9,7 +9,7 @@ function console_log( $data ){
 
 //Your host should be "localhost"
 $db_host = "www.yuetinglu.com";
-$user_name = "yueting1_art";
+$user_name = "yueting1_fine_art";
 $user_password = "luyueting1115";
 $db_name = "yueting1_art_work";
 

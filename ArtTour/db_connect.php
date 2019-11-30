d24f80f9b91bfb22242ab3d4d06d270a68d410b3
yuetinglu <44427526+yuetinglu@users.noreply.github.com>
2019-11-30 01:35:15 -0800

    change db

--- ArtTour/db_connect.php
+++ ArtTour/db_connect.php
@@ -9,9 +9,9 @@ function console_log( $data ){
 
 //Your host should be "localhost"
 $db_host = "www.yuetinglu.com";
-$user_name = "yueting1_fine_art";
+$user_name = "yueting1_art";
 $user_password = "luyueting1115";
-$db_name = "yueting1_art_work";
+$db_name = "yueting1_fine_art";
 
 $conn = new mysqli(
 	$db_host,

62f22fb4993edf2ca1a895f016fb8102c1fec054
yuetinglu <44427526+yuetinglu@users.noreply.github.com>
2019-11-29 22:28:20 -0800

    lite version for bluehost

--- aau/wnm617/midterm/main.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$("#map_canvas").googleMap({
-	  zoom: 12, // Initial zoom level (optional)
-	  coords: [40.7128, 74.0060], // Map center (optional)
-	  type: "ROADMAP" // Map type (optional)
-	});
-    
-    
-    
